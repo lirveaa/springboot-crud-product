6a7d0571eaba49d6b871dd1ba18e63a000a3e06e
Husen Mansurov <lirvess@gmail.com>
2021-06-24 21:59:19 +0900

    Initial commit

---
+++ src/main/java/com/javatechie/springbootcrudexample2/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.javatechie.springbootcrudexample2.repository;
+
+import com.javatechie.springbootcrudexample2.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Product findByName(String name);
+}
